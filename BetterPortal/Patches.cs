07b11216a598278d99f908ee479bc0eb7e892d15
eideehi <41231321+eideehi@users.noreply.github.com>
2022-12-12 00:43:08 +0900

    Improve Portal connection handling
    Connection is updated immediately after changing the tag of the destination.

--- BetterPortal/Patches.cs
+++ BetterPortal/Patches.cs
@@ -1,4 +1,5 @@
 using System;
+using System.Collections;
 using System.Collections.Generic;
 using System.Diagnostics.CodeAnalysis;
 using System.Linq;
@@ -154,6 +155,56 @@ namespace BetterPortal
             _zNetView = null;
         }
 
+        private IEnumerator UpdateConnection()
+        {
+            var zdo = _zNetView.GetZDO();
+            var dest = zdo.GetString("desttag");
+
+            var targetId = zdo.GetZDOID("target");
+            if (!targetId.IsNone())
+            {
+                var target = ZDOMan.instance.GetZDO(targetId);
+                if (target == null || target.GetString("tag") != dest)
+                {
+                    zdo.SetOwner(ZDOMan.instance.GetMyID());
+                    zdo.Set("target", ZDOID.None);
+                    ZDOMan.instance.ForceSendZDO(zdo.m_uid);
+                }
+            }
+
+            if (!zdo.GetZDOID("target").IsNone())
+            {
+                StopCoroutine(nameof(UpdateConnection));
+                yield return null;
+            }
+
+            var portals = new List<ZDO>();
+            var index = 0;
+            bool done;
+            do
+            {
+                done = ZDOMan.instance.GetAllZDOsWithPrefabIterative(
+                    Game.instance.m_portalPrefab.name, portals, ref index);
+                yield return null;
+            } while (!done);
+
+            portals = portals
+                .Where(x => x != zdo && x.GetString("tag") == dest)
+                .ToList();
+            var portal = portals.Count == 0
+                ? null
+                : portals[UnityEngine.Random.Range(0, portals.Count)];
+            if (portal != null)
+            {
+                zdo.SetOwner(ZDOMan.instance.GetMyID());
+                zdo.Set("target", portal.m_uid);
+                ZDOMan.instance.ForceSendZDO(zdo.m_uid);
+            }
+
+            StopCoroutine(nameof(UpdateConnection));
+            yield return null;
+        }
+
         public string GetText()
         {
             return _zNetView.GetZDO().GetString("desttag");
@@ -167,8 +218,10 @@ namespace BetterPortal
 
         private void RPC_SetTagDest(long sender, string tagDest)
         {
-            if (_zNetView.IsValid() && _zNetView.IsOwner() && GetText() != tagDest)
-                _zNetView.GetZDO().Set("desttag", tagDest);
+            if (!_zNetView.IsValid() || !_zNetView.IsOwner() || GetText() == tagDest) return;
+
+            _zNetView.GetZDO().Set("desttag", tagDest);
+            StartCoroutine(nameof(UpdateConnection));
         }
     }
 }
